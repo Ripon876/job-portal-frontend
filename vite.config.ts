8574b184fd3ef777df7c7c94c68926e0c7625974
Ripon876 <islam876ripon@gmail.com>
2024-09-12 02:23:30 +0600

    build: add SASS support to project and add react-router-dom and mantine forms package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react";
+import sass from "sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 });
