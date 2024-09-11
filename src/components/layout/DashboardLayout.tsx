b133cf7bc8855e957c408ba5d7af22508902230c
Ripon876 <islam876ripon@gmail.com>
2024-09-12 04:30:31 +0600

    feat: add nav links in dashboard sidebar

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,33 +1,62 @@
+import { List, ListPlus } from "lucide-react";
 import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Burger, Group, Skeleton } from "@mantine/core";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { AppShell, Burger, Group, NavLink, Title } from "@mantine/core";
 
 type Props = {};
 
-const DashboardLayout = (props: Props) => {
+const DashboardLayout = ({}: Props) => {
   const [opened, { toggle }] = useDisclosure();
+  const location = useLocation();
+
+  // TODO: Check if user is authenticated and get user role
+  const userRole = "admin";
+
+  const items = () => {
+    const navItems = [{ label: "Jobs", link: "/dashboard/jobs", icon: List }];
+
+    if (userRole === "admin") {
+      navItems.push({
+        label: "Add new job",
+        link: "/dashboard/jobs/add",
+        icon: ListPlus,
+      });
+    } else if (userRole === "user") {
+      navItems.push({
+        label: "Applied Jobs",
+        link: "/dashboard/jobs/applied",
+        icon: ListPlus,
+      });
+    }
+
+    return navItems.map((item) => (
+      <NavLink
+        component={Link}
+        to={item.link}
+        key={item.label}
+        active={location.pathname === item.link}
+        label={item.label}
+        leftSection={<item.icon size="1rem" strokeWidth="1.5" />}
+        fz={"lg"}
+        fw={500}
+      />
+    ));
+  };
+
   return (
     <AppShell
       header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      navbar={{ width: 350, breakpoint: "sm", collapsed: { mobile: !opened } }}
       padding="md"
     >
       <AppShell.Header>
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          Job Portal
+          <Title order={2}>Job Portal</Title>
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
-      </AppShell.Navbar>
+      <AppShell.Navbar p="md">{items()}</AppShell.Navbar>
       <AppShell.Main>
-        {" "}
         <Outlet />
       </AppShell.Main>
     </AppShell>
