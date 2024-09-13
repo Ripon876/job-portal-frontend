4cc72130a9935884d11b0eac03a7133036f56184
Ripon876 <islam876ripon@gmail.com>
2024-09-13 08:37:05 +0600

    feat: add AppliedJobsTable component and integrate in with AppliedJobs page

--- src/pages/dashboard/user/AppliedJobs.tsx
+++ src/pages/dashboard/user/AppliedJobs.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { Box, Title } from "@mantine/core";
+import AppliedJobsTable from "@/components/tables/AppliedJobsTable";
 
 type Props = {};
 
-const AppliedJobs = (props: Props) => {
-  return <div>Applied Jobs</div>;
+const AppliedJobs = ({}: Props) => {
+  return (
+    <Box p={"md"} maw={"1024px"} mx={"auto"}>
+      <Title order={2} pb={"md"}>
+        Applied Jobs
+      </Title>
+
+      <AppliedJobsTable />
+    </Box>
+  );
 };
 
 export default AppliedJobs;
