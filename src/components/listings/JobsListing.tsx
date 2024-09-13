f10644358f7f5860684d63f5f673c5c68948a2c9
Ripon876 <islam876ripon@gmail.com>
2024-09-13 08:42:36 +0600

    feat: add pagination to JobsListing component

--- src/components/listings/JobsListing.tsx
+++ src/components/listings/JobsListing.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Box, Grid } from "@mantine/core";
+import { Box, Grid, Pagination } from "@mantine/core";
 import JobsListingSearchBar from "./JobsListingSearchBar";
 import JobCard from "./JobCard";
 
 type Props = {};
 
 const JobsListing = ({}: Props) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [searchQuery, setSearchQuery] = useState("");
   const [filters, setFilters] = useState({
     location: "",
@@ -42,6 +43,13 @@ const JobsListing = ({}: Props) => {
             <JobCard />
           </Grid.Col>
         </Grid>
+        <Box display="flex" style={{ justifyContent: "center" }} my={"3rem"}>
+          <Pagination
+            total={10}
+            value={currentPage}
+            onChange={setCurrentPage}
+          />
+        </Box>
       </Box>
     </Box>
   );
