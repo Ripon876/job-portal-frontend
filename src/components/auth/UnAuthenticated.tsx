933f57a4a3d71f359dab96511675806402af1630
Ripon876 <islam876ripon@gmail.com>
2024-09-14 13:45:19 +0600

    feat: redirect to dashboard page if user if already logged in

---
+++ src/components/auth/UnAuthenticated.tsx
@@ -0,0 +1,49 @@
+import { fetchUser, resetError } from "@/store/auth/authSlice";
+import { AppDispatch, RootState } from "@/store/store";
+import { Flex, Loader } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const UnAuthenticated = ({}: {}) => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch<AppDispatch>();
+  const { isAuthenticated, user, error } = useSelector(
+    (state: RootState) => state.auth
+  );
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(fetchUser());
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setLoading(false);
+    }
+
+    if (error) {
+      setLoading(false);
+      dispatch(resetError());
+    }
+  }, [isAuthenticated, error]);
+
+  if (loading) {
+    return (
+      <Flex justify={"center"} align="center" h="100vh" mih={"100vh"}>
+        <Loader color="blue" />
+      </Flex>
+    );
+  } else {
+    return !isAuthenticated ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/dashboard/jobs" replace />
+    );
+  }
+};
+
+export default UnAuthenticated;
