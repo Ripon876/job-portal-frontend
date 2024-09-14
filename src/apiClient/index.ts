125705d8349e1bddf5f26c69f32d6ea88ff0169c
Ripon876 <islam876ripon@gmail.com>
2024-09-14 12:09:38 +0600

    refactor: update base URL to include API version

--- src/apiClient/index.ts
+++ src/apiClient/index.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_APP_API_BASE;
+const apiVersoin = import.meta.env.VITE_APP_API_VERSION;
 
 export const apiClient = axios.create({
-  baseURL,
+  baseURL: `${baseURL}/api/${apiVersoin}`,
   withCredentials: true,
 });
